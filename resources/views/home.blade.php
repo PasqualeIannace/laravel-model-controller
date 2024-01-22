14eea2283e0a838aca7f451d78ffc9b6f54dbaed
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-22 19:15:23 +0100

    Fine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+<h1>Film</h1>
+<ul>
+@foreach ($movies as $film)
+    <li>
+        <h3>{{$film->title}}</h3>
+    </li>
+@endforeach
+</ul>
 @endsection
